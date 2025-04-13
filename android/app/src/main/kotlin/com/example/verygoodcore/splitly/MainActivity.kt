c72cbd5389578ff518fe25ec96b60dffa90babf0
NicoCieri <nicocieri16@gmail.com>
2025-04-13 17:32:33 -0300

    git init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/splitly/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.splitly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:splitly/app/app.dart';
+import 'package:splitly/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
