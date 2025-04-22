f6734fe95cf1cc73bafed835ae1876cf69387a5b
Nico <102190013+NicoCieri10@users.noreply.github.com>
2025-04-21 23:49:38 -0300

    feat: InitialPage loading animation and title

--- lib/initial/view/initial_page.dart
+++ lib/initial/view/initial_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:sizer/sizer.dart';
 import 'package:splitly/home/home.dart';
+import 'package:splitly/home/widget/widget.dart';
 
 class InitialPage extends StatefulWidget {
   const InitialPage({super.key});
@@ -23,17 +25,33 @@ class _InitialPageState extends State<InitialPage> {
 
     if (!mounted) return;
 
-    context.replaceNamed(HomePage.route);
+    // context.replaceNamed(HomePage.route);
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Splitly',
-          style: TextStyle(fontSize: 20),
-        ),
+    final primaryColor = Theme.of(context).primaryColor;
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 233, 225, 255),
+      body: Column(
+        children: [
+          const Spacer(flex: 2),
+          Text(
+            'Splitly',
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w600,
+              fontSize: 40.sp,
+              color: primaryColor.withAlpha(155),
+            ),
+          ),
+          const Spacer(flex: 4),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.w),
+            child: const LinearProgressIndicator(),
+          ),
+          const Spacer(),
+        ],
       ),
     );
   }
